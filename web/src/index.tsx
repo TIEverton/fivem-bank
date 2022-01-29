939cf8841b7fa6dcd36ddb549dd2c6d2f3b957b0
evertonpinheiro <evertonpinheiroti@gmail.com>
2022-01-29 00:51:36 -0300

    feat: added global style

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+import GlobalStyle from './styles/global';
+
 import { VisibilityProvider } from "./providers/VisibilityProvider";
 
 ReactDOM.render(
   <React.StrictMode>
     <VisibilityProvider>
       <App />
+      <GlobalStyle />
     </VisibilityProvider>
   </React.StrictMode>,
   document.getElementById('root')
